1a6c6e2b913a7eea1a48bfbdc95b9de26b146666
Paul Jones <paul.tyler.jones@gmail.com>
2025-11-29 01:37:12 +0000

    Created 10 properly named variables

---
+++ variable_practice.py
@@ -0,0 +1,22 @@
+# Personal Information Variables
+student_name = "Mr. Jones" #Anything
+student_age = 21
+grade_level = 25
+
+# Academic Variables
+math_score = 89
+science_score = 71
+english_score = 91
+
+# Time & Schedule Variables
+hours_studied = 10
+days_in_week = 7
+
+# Financial Variables
+lunch_money = 200
+book_cost = 900
+
+# Print all variables to verify
+print("Student Name:", student_name)
+print("Age:", student_age)
+# Continue printing remaining variables...
