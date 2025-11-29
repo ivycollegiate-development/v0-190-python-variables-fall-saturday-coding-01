312f988c2dcb08b1e5130b5651b7851ce140c755
NathanealSun <nsun28_student@ivycollegiate.org>
2025-11-29 01:43:25 +0000

    Created 10 properly named variables

---
+++ variable_practice.py
@@ -0,0 +1,21 @@
+# Personal Information Variables
+student name = "Nathaneal Sun"
+student Age = 15
+grade level = 10
+
+# Academy Variables
+math score = 89
+science score = 90
+english score = 95
+
+# Time & Schedule Variables
+hours studied = 10
+days in week = 6
+
+# F i nanci al V ari ab l es
+lunch money = 100
+book cost = 1000
+
+# P ri nt al l v ari ab l es t o veri f y
+print("Student Name:", student_name)
+print("Age:"student_age)
